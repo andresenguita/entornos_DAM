6aaea80de85bc1ab682c9c80c960c0e40500e50b
DAM <DAM@DAM-15>
2024-01-17 10:27:06 +0100

    Primer registro de todo

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,41 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static boolean esVocal (char car) {
+		boolean es_vocal = false;
+		
+		if ((car == 'a') || (car == 'e') || (car == 'i') || (car == 'o') || (car == 'u')) {
+			es_vocal = true;
+		}
+		return es_vocal;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		String frase = "";
+		int contadorVocales = 0;
+
+		System.out.println("Por favor, escriba una frase:");
+		frase = sc.nextLine();
+		for (int i=0; i<frase.length(); i++) {
+			if (esVocal(frase.charAt(i)));
+			contadorVocales ++;
+		}
+		System.out.println("El número de vocales es: " + contadorVocales);
+
+
+		sc.close();
+		System.out.println("Bye!");
+	}
+}
+
+
+
+
+
+
+
+
